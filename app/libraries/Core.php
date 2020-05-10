03ad5c13db371e1f6dc4df60962a68655ae55944
Riot Nicolas <nico.riot@free.fr>
2020-05-10 20:59:16 +0200

    Core

---
+++ app/libraries/Core.php
@@ -0,0 +1,22 @@
+<?php 
+
+//app core classe 
+//creates URL & loads core controller
+//URL FORMAT - /controller/method/params
+
+
+class Core{
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
+    protected $params =[];
+
+
+    public function __construct()
+    {
+        $this->getUrl();
+    }
+
+    public function getUrl(){
+        echo $_GET['url'];
+    }
+}
